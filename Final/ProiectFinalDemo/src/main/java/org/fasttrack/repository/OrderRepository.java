7aae88856d83e1b2899270f8d87602722d6a418c
SergiuIlea <136497712+SergiuIlea@users.noreply.github.com>
2023-10-20 14:25:59 +0300

    Delete Final/ProiectFinalDemo directory

--- Final/ProiectFinalDemo/src/main/java/org/fasttrack/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package org.fasttrack.repository;
-
-import org.fasttrack.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Integer> {
-}
--- Final/ProiectFinalDemo/src/main/java/org/fasttrack/ui/ProductUiController.java
+++
@@ -1,4 +0,0 @@
-package org.fasttrack.ui;
-
-public class ProductUiController {
-}
